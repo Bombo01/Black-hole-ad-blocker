9435391aa2f5f7964e049f873668adcde807a8e0
lucab <lucabombonati31@gmail.com>
2021-07-18 18:40:30 +0200

    Add 'off mode'

--- background.js
+++ background.js
@@ -3,24 +3,36 @@ let patternBack = [];
 chrome.runtime.onInstalled.addListener(function () {
     let url = chrome.runtime.getURL('data/blacklist_normal.json');
     chrome.storage.local.get(["difficulty"], function (items) {
-        console.log(items['difficulty'])
         if (items['difficulty'] === undefined) {
             console.log("SET DIFFICULTY TO NORMAL")
+
             url = chrome.runtime.getURL('data/blacklist_normal.json');
+
             fetch(url)
                 .then((response) => response.json())
                 .then((json) => doSomething(json));
-        }else {
-            if (items['difficulty'].toString() === "1") {
-                console.log("SET DIFFICULTY TO NORMAL")
+        } else {
+            if (items['difficulty'].toString() === "0") {
+                console.log("ADBLOCKER OFF");
+
+                url = chrome.runtime.getURL('data/blacklist_off.json');
+
+                fetch(url)
+                    .then((response) => response.json())
+                    .then((json) => doSomething(json));
+            } else if (items['difficulty'].toString() === "1") {
+                console.log("SET DIFFICULTY TO NORMAL");
+
                 url = chrome.runtime.getURL('data/blacklist_normal.json');
+
                 fetch(url)
                     .then((response) => response.json())
                     .then((json) => doSomething(json));
             } else if (items['difficulty'].toString() === "2") {
-                console.log("SET DIFFICULTY TO AGGRESSIVE")
+                console.log("SET DIFFICULTY TO AGGRESSIVE");
+
                 url = chrome.runtime.getURL('data/blacklist_aggressive.json');
-                console.log(url)
+
                 fetch(url)
                     .then((response) => response.json())
                     .then((json) => doSomething(json));
